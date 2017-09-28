f2049126e58c15d33f3c947c5e4f3fdb8dea3638
Benjie <benjiecodes@gmail.com>
2017-09-28 15:33:47 -0400

    created form on StorePicker Component and imported css styles at index

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-
 class StorePicker extends React.Component {
   render() {
-    return <p>Hello World</p>
+    // JSX Syntax below
+    return (
+
+      <form className ="store-selector">
+        <h2>Please Enter A Store</h2>
+        <input type="text" required placeholder="Store Name" /> { /* JSX Syntax */ }
+        <button type="submit">Visit Store -></button>
+      </form>
+      // Can only run one parent element
+    )
   }
 }
 
