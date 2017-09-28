05533c5f7c3e991f2284ef1033519c19572d5264
Benjie <benjiecodes@gmail.com>
2017-09-28 16:28:48 -0400

    converted Header component to stateless functional component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,27 @@
 import React from 'react';
 
-class Header extends React.Component {
-  render () {
-    return (
-      <header className="top">
-        <h1>
-          Catch
-          <span className="ofThe">
-          <span className="of">of</span>
-          <span className="the">the</span>
-          </span>
-          day
-          </h1>
-        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
-      </header>
-    )
-  }
+/* can also use
+function Header (props) {
+    return code here...
 }
+*/
+
+const Header = (props) => {
+  return (
+    <header className="top">
+      <h1>
+        Catch
+        <span className="ofThe">
+        <span className="of">of</span>
+        <span className="the">the</span>
+        </span>
+        day
+        </h1>
+      <h3 className="tagline"><span>{props.tagline}</span></h3>
+    </header>
+  )
+}
+
+
 
 export default Header;
