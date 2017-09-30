c5809ea60f3d8ae614185d49500792cd9bcc7019
Benjie <benjiecodes@gmail.com>
2017-09-29 22:01:37 -0400

    working with react events figuring out binding for functions to StorePicker Component (used constructor method)

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,13 +2,26 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+  constructor() {
+    super(); // runs React.Component first
+    this.goToStore = this.goToStore.bind(this);
+  }
+  goToStore(event) {
+    event.preventDefault();
+    console.log('You successfully changed the URL')
+    // 1. grab the text from the box
+    // try to modify data and avoid touching the DOM as much as possible
+    console.log(this)
+    // 2. we're going to transition from / to /store/:storeId
+  }
   render() {
     // JSX Syntax below
     return (
-
-      <form className ="store-selector">
+      <form className ="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={ getFunName() }/> { /* JSX Syntax */ }
+        <input type="text" required placeholder="Store Name"
+          defaultValue={ getFunName() }
+          ref={(input) => { this.storeInput = input}} /> { /* JSX Syntax For Commenting*/ }
         <button type="submit">Visit Store -></button>
       </form>
       // Can only run one parent element
