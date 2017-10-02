6ebf19ee06825f8bb9127d30c7d7cecc6cb13bec
Benjie <benjiecodes@gmail.com>
2017-10-02 17:52:33 -0400

    synced the backend "firebase" to my project

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  apiKey: "AIzaSyCjWsmKSMIeyScwgchR73G4S_UjdO0siF8",
+  authDomain: "react-cotd-benjie-songso-71a97.firebaseapp.com",
+  databaseURL: "https://react-cotd-benjie-songso-71a97.firebaseio.com",
+});
+
+export default base;
