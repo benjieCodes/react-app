36ba129f3137485988f069808c1f768c8b4b45e9
Benjie <benjiecodes@gmail.com>
2017-10-02 12:47:51 -0400

    created loadSamples function to append Fish data to our Fish Component and display using JSX

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import { formatPrice } from '../helpers'
 class Fish extends React.Component {
   render () {
+    const { details } = this.props; // to prevent from refering this.props to fish object
+
     return (
-      <li className="menu-fish">🐟</li>
+      <li className="menu-fish">
+        <img src={details.image} alt={details.name} />
+        <h3 className="fish-name">
+          {details.name}
+          <span className="price">{formatPrice(details.price)}</span>
+        </h3>
+        <p>{details.desc}</p>
+
+
+      </li>
     )
   }
 }
